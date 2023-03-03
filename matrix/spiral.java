bcc5323f21866479de751a00648b7d631032b755
Amol Nakhate <106758417+amol64546@users.noreply.github.com>
2023-03-03 12:22:29 +0530

    corrected

--- matrix/spiral.java
+++ matrix/spiral.java
@@ -14,27 +14,29 @@ public class spiral {
 
   public static void spiralOrder(int[][] matrix) {
 
-    int c1 = 0, r1 = 0, c2 = matrix[0].length-1, r2 = matrix.length-1;
+    int c1 = 0, r1 = 0, c2 = matrix[0].length, r2 = matrix.length;
 
-    while (c1 <= c2 && r1 <= r2) {
-      for (int i = c1; i <= c2; i++)
-        ans.add(matrix[r1][i]);
-      r1++;
-
-      for (int i = r1; i <= r2; i++)
-        ans.add(matrix[i][c2]);
-      c2--;
-
-      if (r1 <= r2)
-        for (int i = c2; i >= c1; i--)
-          ans.add(matrix[r2][i]);
-      r2--;
-
-      if (c1 <= c2)
-        for (int i = r2; i >= r1; i--)
-          ans.add(matrix[i][c1]);
-      c1++;
+   while (c1 < c2 && r1 < r2) {
+		 for (int i = r1; i < r2; i++)
+	        ans.add(matrix[i][c1]);
+	      c1++;
+		
+	      for (int i = c1; i < c2; i++)
+	        ans.add(matrix[r2-1][i]);
+	      r2--;
+    
+		if (r1 < r2-1){
+			  for (int i = r2-1; i >= r1; i--)
+		          ans.add(matrix[i][c2-1]);
+		      c2--;
+		  }
+	      if (c1 < c2-1){
+			  for (int i = c2-1; i >= c1; i--)
+		          ans.add(matrix[r1][i]);
+		      r1++;
+		  }
     }
+    
 
   }
 }
